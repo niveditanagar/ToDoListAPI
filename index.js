e5230df944ed02892a8ca1922f487ac0dc411b79
Nivi Nagar <nivinagar@mac.lan>
2026-01-14 21:26:08 -0500

    added hard coded logic for HTTP requests

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const express = require('express')
-const fs = require('fs')
 const app = express()
 const todos = require('./todos.json')
 
-// Middleware to parse JSON request bodies
-app.use(express.json())
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
@@ -16,30 +13,16 @@ app.get('/todos', (req, res) => {
 
 app.post('/todos', (req, res) => {
     const newTodo = {
-        id: todos.todos.length + 1,
-        title: req.body.title,
+        id: 5,
+        title: 'take a nap',
         completed: false
     }
 
     todos.todos.push(newTodo)
 
-    // Write updated todos to the JSON file
-    fs.writeFileSync('./todos.json', JSON.stringify(todos, null, 2))
     res.status(201).json(newTodo)
 })
 
 app.delete('/todos/:id', (req, res) => {
-    const id = parseInt(req.params.id) // Convert id from string to integer
-
-    // Find the index of the todo that matcheds the id
-    const index = todos.todos.findIndex(todo => todo.id === id)
-
-    // Remove the todo from the array
-    const deletedTodo = todos.todos.splice(index, 1)
-
-    // Update todos to the JSON file
-    fs.writeFileSync('./todos.json', JSON.stringify(todos, null, 2))
-    
-    res.status(204).json({ message: 'Todo deleted', deletedTodo: deletedTodo[0] })
-
+    res.status(204).send()
 })
