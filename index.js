c544921c750cfd68722c7c72e0f7ed16f835c0f4
Nivi Nagar <nivinagar@mac.lan>
2026-01-17 18:16:31 -0500

    pushing code up the new branch

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const fs = require('fs')
 const app = express()
 const todos = require('./todos.json')
 
+// Middleware to parse JSON bodies
+app.use(express.json())
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
@@ -13,12 +16,14 @@ app.get('/todos', (req, res) => {
 
 app.post('/todos', (req, res) => {
     const newTodo = {
-        id: 5,
-        text: 'take a nap',
+        id: todos.todos.length +1,
+        text: req.body.text,
         completed: false
     }
-
     todos.todos.push(newTodo)
+
+    // Writes the todos to the JSON file
+    fs.writeFileSync('./todos.json', JSON.stringify(todos, null, 2))
 
     res.status(201).json(newTodo)
 })
